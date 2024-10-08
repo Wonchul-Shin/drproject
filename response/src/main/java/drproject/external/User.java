ed617ac3ae6ffc8444c8e8f788c89c5dc53b51f6
Wonchul-Shin <72932922+Wonchul-Shin@users.noreply.github.com>
2024-10-08 16:03:20 +0900

    commit

---
+++ response/src/main/java/drproject/external/User.java
@@ -0,0 +1,13 @@
+package drproject.external;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer point;
+    private Integer capacity;
+}
